342743e7883b602d7bcf16352be03456f0b9c713
IvanLovas <lovass19@gmail.com>
2021-04-15 20:20:33 +0200

    Create Post model and migration. Add 2 routes which list all posts and a single post by id. Created PostControler

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>{{$post->title}}</title>
+    </head>
+    <body>
+    <a href="/posts">Home</a>
+        <h2>{{$post->title}}</h2>
+        <p>{{$post->body}}</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\DB;
+use App\Models\Post; 
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', [PostControler::class, 'index']);
+
+
+Route::get('/posts/{id}', [PostControler::class, 'show']);
+
+
